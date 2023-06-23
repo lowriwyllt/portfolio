89a5c6eb3424b83fabf8e637be055200976bd551
lowriwyllt <lowriwyllt@live.co.uk>
2023-06-23 11:56:44 +0100

    updating color theme so that the contrast works

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,11 @@ module.exports = {
     extend: {
       colors: {
         darkPurple: "#3C174D",
-        lilac: "#A39FC5",
-        darkBlue: "#29486D",
-        lightBlue: "#2C97A8",
+        lilac: "#B4AFCF",
+        darkBlue: "#19283E",
+        lightBlue: "#5AC3D3",
         cream: "#F2F9FA",
       },
-
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
