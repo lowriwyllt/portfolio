366d819148d99fe3f8d7335fceadd9e7f4d31eae
lowriwyllt <lowriwyllt@live.co.uk>
2023-06-23 11:57:28 +0100

    Adds defaultNavItems - includes all of the main pages on my site and which icons to use

---
+++ app/components/defaultNavItems.tsx
@@ -0,0 +1,32 @@
+// components/layout/defaultNavItems.tsx
+import React from "react";
+import {
+  CodeBracketIcon,
+  DocumentIcon,
+  HomeIcon,
+  GlobeEuropeAfricaIcon,
+} from "@heroicons/react/24/outline";
+import { NavItem } from "./Sidebar";
+
+export const defaultNavItems: NavItem[] = [
+  {
+    label: "Home",
+    href: "/",
+    icon: <HomeIcon className="w-6 h-6" />,
+  },
+  {
+    label: "Coding Projects",
+    href: "/codingprojects",
+    icon: <CodeBracketIcon className="w-6 h-6" />,
+  },
+  {
+    label: "Experience",
+    href: "/experience",
+    icon: <DocumentIcon className="w-6 h-6" />,
+  },
+  {
+    label: "Travel",
+    href: "/travel",
+    icon: <GlobeEuropeAfricaIcon className="w-6 h-6" />,
+  },
+];
