574acd78579c9cc263b0b3dc10b466c889ab8572
lowriwyllt <lowriwyllt@live.co.uk>
2023-06-26 10:22:08 +0100

    changed to landing page - gives me the ability to make my website bilingual

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Home - Lowri Roberts",
-  description:
-    "Welcome to Lowri Roberts' portfolio website. Explore my work as a web designer.",
+  title: "Landing page - Lowri Roberts",
+  description: "",
 };
 
-export default function Home() {
+export default function LandingPage() {
   return (
-    <main className="flex md:min-h-[calc(100vh-108px)] min-h-[calc(100vh-148px)] flex-col items-center justify-between relative top-[100px] px-[56px] py-24">
-      <h1>Home</h1>
-    </main>
+    <main className="flex md:min-h-[calc(100vh-108px)] min-h-[calc(100vh-148px)] flex-col items-center justify-between relative top-[100px] px-[56px] py-24"></main>
   );
 }
