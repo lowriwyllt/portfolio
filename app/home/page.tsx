9525a219d859651cdcd03503bbad56976aac6eda
lowriwyllt <lowriwyllt@live.co.uk>
2023-06-27 15:38:45 +0100

    adds a home page - includes a photo and some text about me - includes my tech stack - includes my recent projects - includes my recent adventures - includes a contact me section

---
+++ app/home/page.tsx
@@ -0,0 +1,43 @@
+import { Metadata } from "next";
+import Image from "next/image";
+import TechStack from "../components/TechStackComponent";
+import RecentProjects from "../components/RecentProjects";
+import RecentAdventures from "../components/RecentAdventures";
+import ContactMe from "../components/ContactMe";
+
+export const metadata: Metadata = {
+  title: "Home - Lowri Roberts",
+  description:
+    "Welcome to Lowri Roberts' portfolio website. Explore my work as a web designer.",
+};
+
+export default function Home() {
+  return (
+    <main
+      className="md:min-h-[calc(100vh-108px)] min-h-[calc(100vh-148px)] h-fit
+     relative top-[100px] pb-[100px]"
+    >
+      <div className="flex flex-col md:flex-row items-center justify-center p-2 pl-8 pr-[60px]">
+        <Image
+          src="/lowri_roberts_working.png"
+          alt="Set up of Lowri Roberts work area.  She is wearing headphones while working on a laptop with a second monitor. She has a ipad on side."
+          width={500}
+          height={500}
+        />
+        <div className="md:p-4">
+          <h1>Lowri Roberts</h1>
+          <p>
+            I'm a full stack web developer, with a focus on JavaScript. I'm
+            dedicated to delivering sleek and dynamic websites.
+          </p>
+        </div>
+      </div>
+      <div className="w-full bg-lilac text-white pl-8">
+        <TechStack />
+        <RecentProjects />
+        <RecentAdventures />
+        <ContactMe />
+      </div>
+    </main>
+  );
+}
