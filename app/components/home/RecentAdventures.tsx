b622b3351802eac75a336c38c9580cb52a2b7a7d
lowriwyllt <lowriwyllt@live.co.uk>
2023-06-27 20:37:30 +0100

    adds recentAdventures from the database - adds these images to the home page - adds the names on top of the images

--- app/components/home/RecentAdventures.tsx
+++ app/components/home/RecentAdventures.tsx
@@ -1,13 +1,23 @@
-export default function RecentAdventures() {
+import { fetchAdventures } from "@/database/database";
+
+export default async function RecentAdventures() {
+  const data = await fetchAdventures();
   return (
     <div className="p-2 flex flex-col items-center pr-[60px]">
       <h2>Recent Adventures</h2>
       <div className="w-full flex flex-col md:flex-row md:h-[70vh] min-h-[70vh] max-w-[1326px] md:w-4/5">
-        <div className="flex max-w-[70vh] h-full grow hover:grow-[7] transition-flex duration-800 ease-in bg-red-700 bg-cover bg-no-repeat bg-center bg-[url('/lowri_roberts_working.png')]"></div>
-        <div className="flex max-w-[70vh] grow h-full hover:grow-[7] transition-flex duration-800 ease-in bg-blue-700 bg-cover bg-no-repeat bg-center bg-[url('/lowri_roberts_working.png')]"></div>
-        <div className="flex max-w-[70vh] grow hover:grow-[7] transition-flex duration-800 ease-in bg-green-700 bg-cover bg-no-repeat bg-center bg-[url('/lowri_roberts_working.png')]"></div>
-        <div className="flex max-w-[70vh] grow  h-full hover:grow-[7] transition-flex duration-800 ease-in bg-black bg-cover bg-no-repeat bg-center bg-[url('/lowri_roberts_working.png')]"></div>
-        <div className="flex max-w-[70vh] grow h-full hover:grow-[7] transition-flex duration-800 ease-in bg-white bg-cover bg-no-repeat bg-center bg-[url('/lowri_roberts_working.png')]"></div>
+        {data.map((adventure) => {
+          return (
+            <div
+              className={`flex justify-center items-center max-w-[70vh] grow hover:grow-[7] transition-flex duration-800 ease-in bg-red-700 bg-cover bg-no-repeat bg-center`}
+              style={{ backgroundImage: `url(${adventure.mainImg})` }}
+            >
+              <h3 className="absolute font-outline-2 drop-shadow-[0_0_10px_rgba(0,0,0,1)] font-bold text-[3em] md:rotate-90">
+                {adventure.name}
+              </h3>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
