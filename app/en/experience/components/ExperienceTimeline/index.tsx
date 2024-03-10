9111602266ae92c62bea3448b7f53c5a33a7567a
lowriwyllt <lowri.roberts@ii.co.uk>
2024-03-10 20:44:15 +0000

    Update ExperienceTimeline component - add company logos to the website

--- app/en/experience/components/ExperienceTimeline/index.tsx
+++ app/en/experience/components/ExperienceTimeline/index.tsx
@@ -24,8 +24,17 @@ export const ExperienceTimeline = async () => {
         <VerticalTimelineElement
           key={index}
           date={`${job.startDate} - ${job.endDate}`}
-          iconStyle={{ background: "#471ca8", color: "#fff" }}
+          iconStyle={{ background: "white", color: "#fff" }}
           contentStyle={{ backgroundColor: "#F2F9FA" }}
+          icon={
+            <div className="rounded-full w-full h-full flex items-center justify-center overflow-hidden">
+              <img
+                src={job.companyImg}
+                alt={`${job.company} logo`}
+                className="rounded-full object-contain"
+              />
+            </div>
+          }
         >
           <h3 className="vertical-timeline-element-title">{job.role}</h3>
           <h4 className="vertical-timeline-element-subtitle">{job.company}</h4>
