50ae298e62866b5f0a98a52ebd9ba66c4740514e
lowriwyllt <lowri.roberts@ii.co.uk>
2024-05-03 17:22:37 +0100

    Refactor Footer component to use Chakra UI components and update styling

--- app/cy/layout.tsx
+++ app/cy/layout.tsx
@@ -2,7 +2,7 @@ import "../globals.css";
 import Layout from "../components/Layout/Layout";
 import Footer from "../components/Layout/Footer";
 import { ibm_plex_mono } from "../fonts";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import { welshNavItems } from "../components/Layout/navItems";
 
 export default function RootLayout({
@@ -14,9 +14,11 @@ export default function RootLayout({
     <html lang="cy" className={ibm_plex_mono.className}>
       <body className="bg-white max-w-[100vw] min-h-fit">
         <ChakraProvider>
-          <Layout language="welsh" />
-          {children}
-          <Footer navItems={welshNavItems} />
+          <Flex direction="column" minH="100vh" justify={"space-between"}>
+            <Layout language="welsh" />
+            {children}
+            <Footer navItems={welshNavItems} />
+          </Flex>
         </ChakraProvider>
       </body>
     </html>
--- app/en/layout.tsx
+++ app/en/layout.tsx
@@ -2,7 +2,7 @@ import "../globals.css";
 import Layout from "../components/Layout/Layout";
 import Footer from "../components/Layout/Footer";
 import { ibm_plex_mono } from "../fonts";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import { englishNavItems } from "../components/Layout/navItems";
 
 export default function RootLayout({
@@ -14,9 +14,11 @@ export default function RootLayout({
     <html lang="en" className={ibm_plex_mono.className}>
       <body className="bg-white max-w-[100vw] min-h-fit">
         <ChakraProvider>
-          <Layout />
-          {children}
-          <Footer navItems={englishNavItems} />
+          <Flex direction="column" minH="100vh" justify={"space-between"}>
+            <Layout />
+            {children}
+            <Footer navItems={englishNavItems} />
+          </Flex>
         </ChakraProvider>
       </body>
     </html>
