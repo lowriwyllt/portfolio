2c4ff07a4c23d906a7435519975c16c24f6e2f8e
lowriwyllt <lowri.roberts@ii.co.uk>
2024-05-03 12:27:02 +0100

    Refactor layout components to use ChakraProvider in RootLayout

--- app/cy/layout.tsx
+++ app/cy/layout.tsx
@@ -3,6 +3,7 @@ import Layout from "../components/Layout/Layout";
 import Footer from "../components/Layout/Footer";
 import { ibm_plex_mono } from "../fonts";
 import { welshNavItems } from "../components/Layout/welshNavItems";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function RootLayout({
   children,
@@ -12,9 +13,11 @@ export default function RootLayout({
   return (
     <html lang="cy" className={ibm_plex_mono.className}>
       <body className="bg-white max-w-[100vw] min-h-fit">
-        <Layout language="welsh" />
-        {children}
-        <Footer navItems={welshNavItems} />
+        <ChakraProvider>
+          <Layout language="welsh" />
+          {children}
+          <Footer navItems={welshNavItems} />
+        </ChakraProvider>
       </body>
     </html>
   );
--- app/en/layout.tsx
+++ app/en/layout.tsx
@@ -3,6 +3,7 @@ import Layout from "../components/Layout/Layout";
 import Footer from "../components/Layout/Footer";
 import { ibm_plex_mono } from "../fonts";
 import { englishNavItems } from "../components/Layout/englishNavItems";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function RootLayout({
   children,
@@ -12,9 +13,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={ibm_plex_mono.className}>
       <body className="bg-white max-w-[100vw] min-h-fit">
-        <Layout />
-        {children}
-        <Footer navItems={englishNavItems} />
+        <ChakraProvider>
+          <Layout />
+          {children}
+          <Footer navItems={englishNavItems} />
+        </ChakraProvider>
       </body>
     </html>
   );
