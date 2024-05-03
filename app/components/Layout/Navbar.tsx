bdd6167ee0817eb343e390ef77df00d720b3c722
lowriwyllt <lowri.roberts@ii.co.uk>
2024-05-03 16:40:28 +0100

    Refactor Navbar component to use Chakra UI and add styling

--- app/components/Layout/Navbar.tsx
+++ app/components/Layout/Navbar.tsx
@@ -1,4 +1,3 @@
-// components/layout/Sidebar.tsx
 import { Link } from "@chakra-ui/next-js";
 import { Button, HStack, Text } from "@chakra-ui/react";
 import { ReactNode } from "react";
@@ -20,6 +19,7 @@ const Navbar = ({ navItems = englishNavItems }: Props): JSX.Element => {
   return (
     <>
       <HStack
+        as={"nav"}
         justify={"end"}
         p={2}
         spacing={2}
