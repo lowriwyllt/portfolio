489bdfd8934ee8fcd0f1179c8816b01460286774
lowriwyllt <lowri.roberts@ii.co.uk>
2026-01-14 18:42:41 +0000

    Refactor code structure for improved readability and maintainability

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        port: "",
-        pathname: "/v0/b/lowri-roberts.appspot.com/**",
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
