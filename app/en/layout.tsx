1ca8a4a1b2b11331ae7c66b03819bbe14448ae92
lowriwyllt <lowri.roberts@ii.co.uk>
2026-01-14 21:51:05 +0000

    feat: add Socials component with social media links and styles

--- app/en/layout.tsx
+++ app/en/layout.tsx
@@ -2,6 +2,7 @@ import "../globals.css";
 import { ibm_plex_mono } from "../fonts";
 import Footer from "../components/Layout/Footer";
 import styles from "./layout.module.css";
+import Socials from "../components/Layout/Socials";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={ibm_plex_mono.className}>
       <body className={styles.body}>
+        <Socials />
         {children}
         <Footer />
       </body>
