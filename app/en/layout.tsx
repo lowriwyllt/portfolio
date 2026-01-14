d0df701c94904ed3cad3b6adcaa432b5f81b860f
lowriwyllt <lowri.roberts@ii.co.uk>
2026-01-14 22:29:07 +0000

    refactor: remove unused Footer and Socials imports from layout

--- app/en/layout.tsx
+++ app/en/layout.tsx
@@ -1,8 +1,6 @@
 import "../globals.css";
 import { ibm_plex_mono } from "../fonts";
-import Footer from "../components/Layout/Footer";
 import styles from "./layout.module.css";
-import Socials from "../components/Layout/Socials";
 import Layout from "../components/Layout";
 
 export default function RootLayout({
