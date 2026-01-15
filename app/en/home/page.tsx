faa14af0e8bd3e3c998940e739840de7f9395aa0
lowriwyllt <lowri.roberts@ii.co.uk>
2026-01-15 20:11:55 +0000

    feat: implement HomePage layout with hero section and TechStack component

--- app/en/home/page.tsx
+++ app/en/home/page.tsx
@@ -1,10 +1,29 @@
 import TechStack from "@/app/components/Home/TechStack";
+import { merriweather } from "@/app/fonts";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 const HomePage = () => {
   return (
     <>
-      <h1>Welcome to the English Home Page</h1>
-      <TechStack />
+      <div className={styles.heroSection}>
+        <Image
+          src="/lowri_roberts_working.png"
+          alt="Set up of Lowri Roberts work area.  She is wearing headphones while working on a laptop with a second monitor. She has a ipad on side."
+          width={500}
+          height={500}
+        />
+        <div className={styles.content}>
+          <h1 className={merriweather.className}>Lowri Roberts</h1>
+          <p>
+            I&apos;m a full stack web developer, with a focus on JavaScript.
+            I&apos;m dedicated to delivering sleek and dynamic websites.
+          </p>
+        </div>
+      </div>
+      <div className={styles.techStackSection}>
+        <TechStack />
+      </div>
     </>
   );
 };
