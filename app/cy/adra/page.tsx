134c7dd49899fb5239465cfbff02ac4682d147b2
lowriwyllt <lowri.roberts@ii.co.uk>
2026-01-16 13:01:15 +0000

    feat: add metadata for HomePage with title and description

--- app/cy/adra/page.tsx
+++ app/cy/adra/page.tsx
@@ -1,4 +1,11 @@
 import HomePageComponent from "@/app/components/Home";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Adra - Lowri Roberts",
+  description:
+    "Croeso i wefan bortffolio Lowri Roberts. Archwiliwch fy ngwaith fel datblygwr gwe.",
+};
 
 const HomePage = () => {
   return <HomePageComponent language="welsh" />;
--- app/en/home/page.tsx
+++ app/en/home/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import HomePageComponent from "@/app/components/Home";
+
+export const metadata: Metadata = {
+  title: "Home - Lowri Roberts",
+  description:
+    "Welcome to Lowri Roberts' portfolio website. Explore my work as a web designer.",
+};
 
 const HomePage = () => {
   return <HomePageComponent />;
