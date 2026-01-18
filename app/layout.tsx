448e5f6b0771c650c6fe3083182111ae48eb36fa
lowriwyllt <lowri.roberts@ii.co.uk>
2026-01-18 16:26:34 +0000

    Refactor RootLayout component to include language detection and update HTML structure for improved accessibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import Layout from "@/app/components/Layout";
+import { Providers } from "./components/Providers";
+import { ibm_plex_mono } from "./fonts";
 import "./globals.css";
+
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
-}): React.ReactNode {
-  return children;
+}) {
+  const pathname = usePathname();
+  const lang = pathname.split("/")?.[1] === "cy" ? "cy" : "en";
+
+  return (
+    <html lang={lang}>
+      <body className={ibm_plex_mono.className}>
+        <Providers>
+          <Layout lang={lang}>{children}</Layout>
+        </Providers>
+      </body>
+    </html>
+  );
 }
